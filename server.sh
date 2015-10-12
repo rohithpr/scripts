68d89e12aa4565ee3689c7c38494cebc9d110591
rohithpr <praroh2@gmail.com>
2015-10-12 10:49:54 +0530

    Run clouder-lite from virtual env

--- server.sh
+++ server.sh
@@ -1,3 +1,7 @@
 python3 ~/code/bookmark-manager/bm/manage.py runserver 0.0.0.0:9000 >> ~/bmlogs 2>&1 &
-python3 ~/code/clouder-lite/server.py me >> ~/cllogs 2>&1 &
 python3 ~/scripts/notifier.py &
+
+# Activate venv for clouder-lite
+source ~/code/clouder-lite/venv/bin/activate
+python3 ~/code/clouder-lite/server.py me >> ~/cllogs 2>&1 &
+deactivate
