9acaf08d6e14c95fc9a884cbc37ca2e72b15c2d3
rohithpr <praroh2@gmail.com>
2015-10-13 14:39:27 +0530

    Fix for zip directory

--- server.sh
+++ server.sh
@@ -2,6 +2,8 @@ python3 ~/code/bookmark-manager/bm/manage.py runserver 0.0.0.0:9000 >> ~/bmlogs 
 python3 ~/scripts/notifier.py &
 
 # Activate venv for clouder-lite
-source ~/code/clouder-lite/venv/bin/activate
-python3 ~/code/clouder-lite/server.py me >> ~/cllogs 2>&1 &
+cd ~/code/clouder-lite
+source venv/bin/activate
+python3 server.py me >> ~/cllogs 2>&1 &
 deactivate
+cd ~
